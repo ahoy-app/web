d9ff849dc6a4bb54983645ee6b6d2dafee55c9fc
SureSRM <sergiozz95@gmail.com>
2019-05-19 13:05:02 +0200

    Config env variables

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,4 +1,4 @@
-export const API_HOST = process.env.API_HOST || 'localhost:8080'
+export const API_HOST = process.env.REACT_APP_API_HOST || 'localhost:8080'
 export const API_HTTP_ENDPOINT = `http://${API_HOST}`
 
 const headers = extras => ({
